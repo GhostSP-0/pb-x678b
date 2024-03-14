5fc3b68d5039ac065c3fdbb72921d985a46ee68f
ghostSP <160274473+GhostSP-0@users.noreply.github.com>
2024-03-14 08:42:51 -0700

    Update twrp_X678B.mk

--- twrp_X678B.mk
+++ twrp_X678B.mk
@@ -25,7 +25,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from our custom product configuration.
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device specific configs.
 $(call inherit-product, device/infinix/X678B/device.mk)
